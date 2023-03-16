c20c4b50e2a638ffbce25f5fe6dfe51f543bb11f
James Linkous <james.william.linkous@gmail.com>
2023-03-16 10:32:49 -0500

    MVP met

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,8 @@
+import { useEffect, useState } from "react"
+import { getAllChezzes } from "../../managers/ChezManager"
+
 export const Home = ({ setToggle}) => {
+
     return <section 
     className="component_container"
     onClick={()=>{setToggle(true)}}>
